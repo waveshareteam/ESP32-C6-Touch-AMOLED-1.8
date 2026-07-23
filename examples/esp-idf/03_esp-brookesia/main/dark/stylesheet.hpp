98b32237c1cbf61984367105a2f2dbf4bbb60835
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2026-07-23 14:29:23 +0800

    fix: remove duplicate Brookesia stylesheet macro

--- examples/esp-idf/03_esp-brookesia/main/dark/stylesheet.hpp
+++ examples/esp-idf/03_esp-brookesia/main/dark/stylesheet.hpp
@@ -66,5 +66,3 @@ constexpr Stylesheet STYLESHEET_368_448_DARK = {
 };
 
 } // namespace esp_brookesia::systems::phone
-
-#define ESP_BROOKESIA_PHONE_DEFAULT_DARK_STYLESHEET() esp_brookesia::systems::phone::STYLESHEET_368_448_DARK
